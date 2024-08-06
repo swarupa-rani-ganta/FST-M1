e2d2290a02fd4f50c6ecd3950ad8fc2356b8e422
swarupa-rani-ganta <Swarupa.Rani.Ganta@ibm.com>
2024-08-06 22:28:27 +0530

    Add files via upload

---
+++ API/Activities/Screenshot.java
@@ -0,0 +1,80 @@
+package Activities;
+
+public class Screenshot import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Screenshot {
+    static AndroidDriver driver;
+
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.miui.calculator");
+        options.setAppActivity(".cal.CalculatorActivity");
+        options.noReset();
+
+        // Server URL
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
+
+        // Driver initialization
+        driver = new AndroidDriver(serverURL, options);
+    }
+
+    @Test
+    public void testMethod() throws IOException {
+        String result = "10";
+        try {
+            // Test step that fails
+            driver.findElement(AppiumBy.id("digit_5")).click();
+        } catch (NoSuchElementException ex) {
+            // Take screenshot when test fails
+            takeScreenshot();
+        }
+
+        // Assertions
+        Assert.assertEquals(result, "10");
+    }
+
+    public static void takeScreenshot() throws IOException {
+        // Take screenshot
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        // This specifies the location the screenshot will be saved
+        File screenShotName = new File("src/test/resources/screenshot.jpg");
+
+        // This will copy the screenshot to the file created
+        FileUtils.copyFile(scrFile, screenShotName);
+
+        // Set filepath for image
+        String filePath = "../" + screenShotName;
+
+        // Set image HTML in the report
+        String path = "<img src='" + filePath + "'/>";
+
+        // Show image in report
+        Reporter.log(path);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
